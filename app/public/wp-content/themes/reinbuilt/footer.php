986b14c310f3f5389e668c81b990b90b68da5163
Gunther <50387432+guntherb7@users.noreply.github.com>
2023-12-26 17:02:18 -0500

    working on nav drawer

--- app/public/wp-content/themes/reinbuilt/footer.php
+++ app/public/wp-content/themes/reinbuilt/footer.php
@@ -4,44 +4,30 @@
  *
  * @package reinbuilt
  * @author Gunther Beam LLC
-**/
+ **/
 $phone = get_field('phone', 'options');
 $email = get_field('email', 'options');
 $address = get_field('address', 'options');
 $addressLink = get_field('address_link', 'options');
 
 ?>
-<footer class="footer">
-    <div class="footer--nav">
-        <ul>
-            <li><a href="http://reinbuilt.local/" class="nav-link">Architecture</a></li>
-            <li><a href="#" class="nav-link">Interior</a></li>
-            <li><a href="#" class="nav-link">Restoration</a></li>
-            <li><a href="#" class="nav-link">Calculators</a></li>
-            <li><a href="#" class="nav-link">Services</a></li>
-            <li><a href="#" class="nav-link">Contact</a></li>
-        </ul>
-    </div>
 
-    <img src="https://reinbuilt.local/wp-content/uploads/2023/12/LOGO.png" alt="" class="footer--logo">
-</footer>
 <!-- Schema JSON-LD -->
 <?php wp_footer(); ?>
 <script>
-const lenis = new Lenis();
+  const lenis = new Lenis();
 
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
 
-function raf(time) {
-  lenis.raf(time);
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
   requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
 </script>
 </body>
+
 </html>
-
-
--- app/public/wp-content/themes/reinbuilt/header.php
+++ app/public/wp-content/themes/reinbuilt/header.php
@@ -4,57 +4,62 @@
  *
  * @package reinbuilt
  * @author Gunther Beam LLC
-**/
-?><!DOCTYPE html>
+ **/
+?>
+<!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
+
 <head>
-<!-- Google Tag Manager -->
+  <!-- Google Tag Manager -->
 
 
-<!-- End Google Tag Manager -->
-<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://indestructibletype.com/fonts/Jost.css" type="text/css" charset="utf-8" />
-<link href="https://fonts.googleapis.com/css2?family=Syncopate&display=swap" rel="stylesheet">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1.0.29/bundled/lenis.min.js"></script> 
-
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<?php wp_head(); ?>
+  <!-- End Google Tag Manager -->
+  <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://indestructibletype.com/fonts/Jost.css" type="text/css" charset="utf-8" />
+  <link href="https://fonts.googleapis.com/css2?family=Syncopate&display=swap" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1.0.29/bundled/lenis.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>
+    <?php wp_title('|', true, 'right'); ?>
+  </title>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<header>
-		<div id="header_container" class="container-full">
-			<div id="header_grid">
-				<div id="header_left">
-					<?php the_custom_logo(); ?>
-				</div>
-				
-				<div id="header_right">
-						<?php
-							$args = array(
-								'container' => false,
-								'theme_location' => 'header-nav'
-							);
-							wp_nav_menu( $args );
-						?>			
-					<div id="header_mobile">
-						<a href="#" id="menu-icon" class="closed"><hr><hr><hr></a>
-						<div id="slide-nav">
-							<?php
-								$args = array(
-									'container' => false,
-									'theme_location' => 'header-nav'
-								);
-								wp_nav_menu( $args );
-							?>		
-						</div>	
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+  <header>
+    <div id="header_right">
+      <a href="javascript:void(0);" class="icon" id="hamburger-menu"><i class="fa fa-bars"></i></button></a>
+    </div>
+  </header>
+  <main>
+  </main>
+  <nav id="bottom-nav">
+    <footer class="footer">
+      <div class="footer--nav">
+        <ul>
+          <li><a href="http://reinbuilt.local/" class="nav-link">Architecture</a></li>
+          <li><a href="#" class="nav-link">Interior</a></li>
+          <li><a href="#" class="nav-link">Restoration</a></li>
+          <li><a href="#" class="nav-link">Calculators</a></li>
+          <li><a href="#" class="nav-link">Services</a></li>
+          <li><a href="#" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+
+      <img src="https://reinbuilt.local/wp-content/uploads/2023/12/LOGO.png" alt="" class="footer--logo">
+    </footer>
+  </nav>
+
+  <script>
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const bottomNav = document.getElementById('bottom-nav');
+
+    hamburgerMenu.addEventListener('click', () => {
+      bottomNav.classList.toggle('open');
+    });
+  </script>
