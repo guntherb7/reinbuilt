09d9366af35b538986a096b837806a6e4a73760e
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-01-26 16:09:34 -0500

    trying to toggle dark mode icons but disappearing

--- app/public/wp-content/themes/reinbuilt/assets/js/src/scripts.js
+++ app/public/wp-content/themes/reinbuilt/assets/js/src/scripts.js
@@ -11,12 +11,30 @@
 var darkModeButton = document.querySelector(".darkMode");
 
 // Add a click event listener to the button
-darkModeButton.addEventListener("click", darkMode);
-
+if (darkModeButton) {
+  darkModeButton.addEventListener("click", darkMode);
+}
 // The darkMode function remains the same
 function darkMode() {
-  var element = document.body;
+  const element = document.body;
+  const nav = document.querySelector(".menu-items");
+
   element.classList.toggle("dark-mode");
+  nav.classList.toggle("dark-mode");
+  // Get the background color of the body element
+  var bodyBackgroundColor = getComputedStyle(document.body).backgroundColor;
+
+  // Check if the background color is #f4eee9
+  if (bodyBackgroundColor === "rgb(244, 238, 233)") {
+    // Hide the .light-icon element
+    document.querySelector(".light-icon").style.display = "none";
+    document.querySelector(".dark-icon").style.display = "block";
+  } else if (bodyBackgroundColor === "rgb(34, 34, 34)") {
+    // Check if the background color is #222
+    // Hide the .dark-icon element
+    document.querySelector(".dark-icon").style.display = "none";
+    document.querySelector(".light-icon").style.display = "block";
+  }
 }
 
 // Smooth Scroll library
