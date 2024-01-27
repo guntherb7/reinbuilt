a6f373d387c311ff6d795dd264f03af9925360d9
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-01-27 16:36:58 -0500

    everything working, cleaning up and refactoring

--- app/public/wp-content/themes/reinbuilt/assets/js/src/scripts.js
+++ app/public/wp-content/themes/reinbuilt/assets/js/src/scripts.js
@@ -7,35 +7,39 @@
 /*jshint esversion: 6 */
 
 // Dark Mode Toggle
-// Get the button element by its class
 var darkModeButton = document.querySelector(".darkMode");
 
-// Add a click event listener to the button
 if (darkModeButton) {
   darkModeButton.addEventListener("click", darkMode);
 }
-// The darkMode function remains the same
+
 function darkMode() {
   const element = document.body;
   const nav = document.querySelector(".menu-items");
+  const lightIcon = document.querySelector(".light-icon");
+  const darkIcon = document.querySelector(".dark-icon");
 
   element.classList.toggle("dark-mode");
   nav.classList.toggle("dark-mode");
-  // Get the background color of the body element
-  var bodyBackgroundColor = getComputedStyle(document.body).backgroundColor;
 
-  // Check if the background color is #f4eee9
-  if (bodyBackgroundColor === "rgb(244, 238, 233)") {
-    // Hide the .light-icon element
-    document.querySelector(".light-icon").style.display = "none";
-    document.querySelector(".dark-icon").style.display = "block";
-  } else if (bodyBackgroundColor === "rgb(34, 34, 34)") {
-    // Check if the background color is #222
-    // Hide the .dark-icon element
-    document.querySelector(".dark-icon").style.display = "none";
-    document.querySelector(".light-icon").style.display = "block";
+  if (element.classList.contains("dark-mode")) {
+    lightIcon.style.display = "none";
+    darkIcon.style.display = "block";
+  } else {
+    lightIcon.style.display = "block";
+    darkIcon.style.display = "none";
   }
 }
+
+window.addEventListener("DOMContentLoaded", function initializeDarkMode() {
+  var bodyBackgroundColor = getComputedStyle(document.body).backgroundColor;
+
+  if (bodyBackgroundColor === "rgb(244, 238, 233)") {
+    document.querySelector(".light-icon").style.display = "block";
+  } else if (bodyBackgroundColor === "rgb(34, 34, 34)") {
+    document.querySelector(".dark-icon").style.display = "block";
+  }
+});
 
 // Smooth Scroll library
 const lenis = new Lenis();
