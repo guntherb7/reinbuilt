07e77febe716785469f8fb7ff62ff48543be0f93
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-01-27 20:08:32 -0500

    removing wordfence for building

--- app/public/wp-content/themes/reinbuilt/index.php
+++ app/public/wp-content/themes/reinbuilt/index.php
@@ -1,29 +1,37 @@
 <?php
 /**
- * 
  * @package reinbuilt
  * @author Gunther Beam LLC
  */
 
+// Define query arguments
 $args = array(
 	'post_type' => 'post',
-	'post_per_page' => '10',
+	'posts_per_page' => 10,
 	'post_status' => 'publish',
 	'order' => 'DESC',
 );
+
+// Create a new instance of WP_Query
 $the_query = new WP_Query($args);
-get_header(); ?>
+
+// Include the header
+get_header();
+?>
 
 <div id="blog">
 	<div class="container blog-posts">
 		<div id="blog-banner">
-			<img src="http://localhost:10020/wp-content/uploads/2024/01/REINBUILT_LOGO_DARK-GRAY.svg"
+			<!-- Use esc_url() for secure output of URLs -->
+			<img src="<?php echo esc_url(home_url('/wp-content/uploads/2024/01/REINBUILT_LOGO_DARK-GRAY.svg')); ?>"
 				alt="Reinbuilt Logo" class="hero--logo nav--logo logo">
 		</div>
 		<h1>Hello World</h1>
-
 	</div>
-
 	<!-- #content -->
+</div>
 
-	<?php get_footer(); ?>
+<?php
+// Include the footer
+get_footer();
+?>
