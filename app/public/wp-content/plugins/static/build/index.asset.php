654604effe41a251a670437994423bef5663fdcc
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-01-29 16:09:12 -0500

    Creating block plugin

---
+++ app/public/wp-content/plugins/static/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd570af0af20cb9460511');
---
+++ app/public/wp-content/plugins/static/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bf8dccc35107895d5854');
