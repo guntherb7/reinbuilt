8df873de9ee0672930b3d0f4db2a8f78e31c8e5b
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-01-30 10:11:58 -0500

    Revert "going to try to reset"
    This reverts commit e6f98e658d69e990329ff9897798c087fc2e3be5.

--- app/public/wp-content/themes/reinbuilt/functions.php
+++ app/public/wp-content/themes/reinbuilt/functions.php
@@ -19,6 +19,14 @@ require_once dirname(__FILE__) . '/includes/team.php'; // Custom Post Type Case 
  */
 
 
+// //Remove Gutenberg Block Library CSS from loading on the frontend
+// function smartwp_remove_wp_block_library_css()
+// {
+// 	wp_dequeue_style('wp-block-library');
+// 	wp_dequeue_style('wp-block-library-theme');
+// 	wp_dequeue_style('wc-blocks-style'); // Remove WooCommerce block CSS
+// }
+// add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
 function reinbuilt_register_acf_blocks()
 {
 	/**
