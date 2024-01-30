fec8e37ba4537171ef02f8b0ff2edf53494b4c35
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-01-30 10:12:09 -0500

    Revert "trying to get back"
    This reverts commit 417ed6a4406fb2eb9cc68db6bf6704b5cbf353b3.

--- app/public/wp-content/themes/reinbuilt/front-page-architecture.php
+++ app/public/wp-content/themes/reinbuilt/front-page-architecture.php
@@ -2,56 +2,89 @@
 /**
  * @package reinbuilt
  * @author Gunther Beam LLC
- */
+ **/
+get_header(); ?>
 
-// Define query arguments
-
-
-// Include the header
-get_header();
-?>
-<!-- <div class="content">
+<div id="content">
     <h1>Hello world</h1>
-Use esc_url() for secure output of URLs
-    <img src="<?php echo esc_url(home_url('/wp-content/uploads/2024/01/REINBUILT_LOGO_DARK-GRAY.svg')); ?>"
-        alt="Reinbuilt Logo" class="hero--logo logo dark-icon">
-    <img src="<?php echo esc_url(home_url('/wp-content/uploads/2024/01/REINBUILT_LOGO_LIGHT-GRAY.svg')); ?>"
-        alt="Reinbuilt Logo" class="hero--logo logo light-icon">
-</div> -->
-<!-- Hero Image -->
-<section id="hero">
-    <div class="wp-block-cover alignfull" style="min-height:800px">
-        <span aria-hidden="true" class="wp-block-cover__background has-background-dim-60 has-background-dim"></span>
-        <img class="wp-block-cover__image-background" alt="" src="https://s.w.org/images/core/5.8/forest.jpg"
-            data-object-fit="cover" />
-        <div class="wp-block-cover__inner-container">
-            <div class="wp-block-align-center">
-                <!-- <img
-                    src="<?php echo esc_url(home_url('/wp-content/uploads/2024/01/REINBUILT_LOGO_DARK-GRAY.svg')); ?>"
-                    alt="Reinbuilt Logo" class="hero--logo logo dark-icon">-->
-                <img src="<?php echo esc_url(home_url('/wp-content/uploads/2024/01/REINBUILT_LOGO_LIGHT-GRAY.svg')); ?>"
-                    alt="Reinbuilt Logo" class="hero--logo logo light-icon">
+    <?php if (has_post_thumbnail()) {
+        $backgroundImg = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
+        <div id="fp-hero" class="hero-bg-fade"
+            style="background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 100%),  url('<?php echo esc_url($backgroundImg[0]); ?>') no-repeat; background-size:114vh; background-position:top right;">
+        <?php } ?>
+        <div id="fp-hero_grid" class="container-full">
+            <div id="fp-hero_right">
+                <div id="fp-hero_right-container">
+                    <?php $image = get_field('hero_image');
+                    if (!empty($image)): ?>
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"
+                            class="orange-highlight img-responsive" />
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div id="fp-hero_left">
+                <h4>
+                    <?php the_field('tagline'); ?>
+                </h4>
+                <h1>
+                    <?php the_field('title'); ?>
+                </h1>
+                <p>
+                    <?php the_field('text'); ?>
+                </p>
+                <?php $link = get_field('link');
+                if ($link):
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    ?>
+                    <a class="btn btn-orange" href="<?php echo esc_url($link_url); ?>">
+                        <?php echo esc_html($link_title); ?>
+                    </a>
+                <?php endif; ?>
+                <div id="fp-hero_social">
+                    <?php get_template_part('block', 'social'); ?>
+                </div>
             </div>
         </div>
     </div>
-</section>
-<!-- Hero END -->
 
-<!--Contact-->
-<section id="contact">
-    <!-- wp:media-text {"align":"full","mediaType":"image","verticalAlignment":"center"} -->
-    <div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-center">
-        <figure class="wp-block-media-text__media"><img src="https://s.w.org/images/core/5.8/architecture-04.jpg"
-                alt="Close-up, abstract view of architecture." /></figure>
-        <!-- wp:forminator/forms {"module_id":"123"} -->
-        <div class="forminator-guttenberg">[forminator_form id="123"]</div>
-        <!-- /wp:forminator/forms -->
+    <div id="fp-grid">
+        <div id="fp-grid_content" class="container-full">
+            <?php if (have_rows('grid')): ?>
+                <?php while (have_rows('grid')):
+                    the_row();
+                    $gridIcon = get_sub_field('icon');
+                    $gridText = get_sub_field('text');
+                    $gridLink = get_sub_field('link');
+                    ?>
+                    <a href="<?php echo esc_url($gridLink); ?>" class="fp-grid_link">
+                        <img src="<?php echo esc_url($gridIcon['url']); ?>" alt="<?php echo esc_attr($gridIcon['alt']); ?>" />
+                        <h4>
+                            <?php echo esc_html($gridText); ?>
+                        </h4>
+                    </a>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
     </div>
-    <!-- /wp:media-text -->
-</section>
-<!--Contact END-->
-<!-- /wp:cover -->
-<?php
-// Include the footer
-get_footer();
-?>
+
+    <div id="fp-cta">
+        <div id="fp-cta_container" class="container">
+            <div id="fp-cta_left">
+                <?php the_field('cta_left'); ?>
+            </div>
+            <div id="fp-cta_right">
+                <?php the_field('cta_right'); ?>
+            </div>
+        </div>
+    </div>
+
+    <div id="fp-quote">
+        <div id="fp-quote_container" class="container-full">
+            <?php the_field('quote'); ?>
+        </div>
+    </div>
+
+</div><!-- #front-page -->
+
+<?php get_footer(); ?>
