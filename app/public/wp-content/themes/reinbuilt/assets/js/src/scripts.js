9aaaefac14be45dbd88413e090eaa188d73ba316
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-02-18 12:56:20 -0500

    Update Swiper styles and scripts

--- app/public/wp-content/themes/reinbuilt/assets/js/src/scripts.js
+++ app/public/wp-content/themes/reinbuilt/assets/js/src/scripts.js
@@ -63,3 +63,43 @@ var darkModeButton = document.querySelector(".darkMode");
 if (darkModeButton) {
   darkModeButton.addEventListener("click", darkMode);
 }
+
+// Swiper JS
+const swiper = new Swiper(".mySwiper", {
+  lazy: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: { slidesPerView: 1, spaceBetween: 20 },
+    600: { slidesPerView: 2, spaceBetween: 20 },
+    968: { slidesPerView: 3, spaceBetween: 20 },
+    1200: { slidesPerView: 4, spaceBetween: 20 },
+  },
+});
+
+const heroSwiper = new Swiper(".hero-swiper", {
+  cssMode: false,
+  setWrapperSize: true,
+  grabCursor: true,
+  mousewheel: true,
+  keyboard: true,
+  longSwipes: true,
+  longSwipesRatio: 0.25,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: true,
+  },
+  breakpoints: {
+    851: {
+      mousewheel: false,
+      keyboard: false,
+      watchSlidesProgress: true,
+      watchSlidesVisibility: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+    },
+  },
+});
