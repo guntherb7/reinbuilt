bd6082f26e8af2a6c47df3fddfdbd3fc884c370b
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-02-18 13:26:37 -0500

    Refactor navigation and dark mode functionality

--- app/public/wp-content/themes/reinbuilt/assets/js/src/nav.js
+++ app/public/wp-content/themes/reinbuilt/assets/js/src/nav.js
@@ -8,20 +8,3 @@
 /*jslint white: true */
 /*jshint browser: true, jquery: true */
 /*jshint esversion: 6 */
-
-// When #menu is clicked, expand the menu and cover the screen with the overlay
-jQuery(document).ready(function ($) {
-  $("#menu").click(function () {
-    $(".menu-items").toggleClass("open");
-    $(".overlay").toggleClass("open");
-  });
-  $(".overlay").click(function () {
-    $(".menu-items").toggleClass("open");
-    $(".overlay").toggleClass("open");
-  });
-});
-// When the window is resized, close the menu and overlay
-$(window).resize(function () {
-  $(".menu-items").removeClass("open");
-  $(".overlay").removeClass("open");
-});
