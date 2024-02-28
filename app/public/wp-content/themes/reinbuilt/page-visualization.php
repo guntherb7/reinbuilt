3a40627bcea61b2e32a61c59af6f8537df34cc3a
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-02-28 16:27:53 -0500

    Add cursor pointer to hero logo

--- app/public/wp-content/themes/reinbuilt/page-visualization.php
+++ app/public/wp-content/themes/reinbuilt/page-visualization.php
@@ -232,7 +232,52 @@
 <section>
 	<img src="/wp-content/uploads/2024/02/Full-n_orange_drive-37-1-jpg.webp" loading="lazy" alt="placeholder"
 		width=“415” height=“300” />
-	<a href="/visualize" role="button" class="visualize-button">Click here to start your project</a>
+	<div class="contact">
+		<div id="mc_embed_shell">
+			<link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+			<div id="mc_embed_signup">
+				<form
+					action="https://guntherbeam.us22.list-manage.com/subscribe/post?u=c84950591afda259787f3a3ba&amp;id=9d1eaef23a&amp;f_id=0070c2e1f0"
+					method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+					target="_blank">
+					<div id="mc_embed_signup_scroll">
+						<h2 class="text-center">Start Building With Us</h2>
+						<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+						<div class="mc-field-group"><label for="mce-EMAIL">Email Address <span
+									class="asterisk">*</span></label><input type="email" name="EMAIL"
+								class="required email" id="mce-EMAIL" required="" value=""><span
+								id="mce-EMAIL-HELPERTEXT" class="helper_text"></span></div>
+						<div class="mc-field-group"><label for="mce-FNAME">First Name <span
+									class="asterisk">*</span></label><input type="text" name="FNAME"
+								class="required text" id="mce-FNAME" required="" value=""></div>
+						<div class="mc-field-group"><label for="mce-LNAME">Last Name <span
+									class="asterisk">*</span></label><input type="text" name="LNAME"
+								class="required text" id="mce-LNAME" required="" value=""></div>
+						<div class="mc-field-group"><label for="mce-PHONE">Phone Number </label><input type="text"
+								name="PHONE" class="REQ_CSS" id="mce-PHONE" value=""></div>
+						<div id="mce-responses" class="clear foot">
+							<div class="response" id="mce-error-response" style="display: none;"></div>
+							<div class="response" id="mce-success-response" style="display: none;"></div>
+						</div>
+						<div aria-hidden="true" style="position: absolute; left: -5000px;">
+							/* real people should not fill this in and expect good things - do not remove this or risk
+							form bot signups */
+							<input type="text" name="b_c84950591afda259787f3a3ba_9d1eaef23a" tabindex="-1" value="">
+						</div>
+						<div class="clear foot">
+							<input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button"
+								value="Start Your Project">
+						</div>
+					</div>
+				</form>
+			</div>
+			<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+			<script
+				type="text/javascript">(function ($) { window.fnames = new Array(); window.ftypes = new Array(); fnames[0] = 'EMAIL'; ftypes[0] = 'email'; fnames[1] = 'FNAME'; ftypes[1] = 'text'; fnames[2] = 'LNAME'; ftypes[2] = 'text'; fnames[4] = 'PHONE'; ftypes[4] = 'phone'; fnames[3] = 'ADDRESS'; ftypes[3] = 'address'; fnames[5] = 'BIRTHDAY'; ftypes[5] = 'birthday'; }(jQuery)); var $mcj = jQuery.noConflict(true);</script>
+		</div>
+
+		<!-- <a href="/visualization" role="button" class="visualize-button dark-mode">Click here to start your project</a> -->
+	</div>
 </section>
 
 <?php get_footer(); ?>
