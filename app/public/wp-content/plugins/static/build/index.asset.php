459299c0fbc43d99d147a1286f148c3347fdb35a
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-02-29 12:09:10 -0500

    matching plugins

--- app/public/wp-content/plugins/static/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0dfecd6741e0b9f0ba34');
--- app/public/wp-content/plugins/static/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'bf8dccc35107895d5854');
