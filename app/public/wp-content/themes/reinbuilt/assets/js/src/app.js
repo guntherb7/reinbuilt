e95ded0ddd84bdb4a6e32f5a89bef1ee532723df
Gunther <50387432+guntherb7@users.noreply.github.com>
2024-04-10 21:02:43 -0400

    Update start-project class in _pages.scss, _footer.scss, _global.scss, _header.scss, _swiper.scss, updated gsap

--- app/public/wp-content/themes/reinbuilt/assets/js/src/app.js
+++ app/public/wp-content/themes/reinbuilt/assets/js/src/app.js
@@ -12,6 +12,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     opacity: 1,
     visibility: "visible",
     duration: 1,
+    ease: "power2.out",
+    scrollTrigger: { trigger: ".start-project", start: "bottom bottom" },
+  });
+
+  gsap.to(".start-project-logo--start", {
+    opacity: 0,
+    visibility: "hidden",
+    duration: 0.6,
     scrollTrigger: { trigger: ".start-project", start: "bottom bottom" },
   });
 });
